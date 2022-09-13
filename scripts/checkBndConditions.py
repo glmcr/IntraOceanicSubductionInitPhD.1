59a2054f0f33ade0a110907e08b023095c466c2a
glmcr <gmj273@gmail.com>
2022-09-13 10:11:28 -0400

    Added script to check for sides boundary conditions.

---
+++ scripts/checkBndConditions.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import sys
+
+velcmyLHS= -1.0
+velcmyRHS= 1.0
+cm2m=0.01
+year=1
+bottomDepth=700e3
+xdim= 1500e3
+switchDepthRHS=140e3
+switchDepthLHS=140e3
+
+#maxDepth= 700e3
+nbCellsY= 700
+
+csvFileName= sys.argv[1]
+
+csvFile= open(csvFileName,"w")
+
+csvFile.write("lhsXVelo,rhsXVelo,yElev\n")
+
+for cellY in range(700):
+
+    y= cellY*1000.0
+
+    lhsXVelo=rhsXVelo= 0.0
+    
+    if (y > (bottomDepth-switchDepthRHS)):
+        rhsXVelo= velcmyRHS*(cm2m/year)
+        
+    elif ( y >= (bottomDepth-switchDepthRHS-20e3)):
+        rhsXVelo= velcmyRHS*(cm2m/year)*((y - (bottomDepth-switchDepthRHS-20e3))*35.0)/bottomDepth
+           
+    elif ( y >= (bottomDepth-switchDepthRHS-30e3)):
+        rhsXVelo= 0.01*velcmyRHS*(cm2m/year)*((y - (bottomDepth-switchDepthRHS-20e3))*35.0)/bottomDepth
+           
+    else:
+        rhsXVelo= -5e-3*velcmyRHS*(cm2m/year) 
+
+    if (y > (bottomDepth-switchDepthLHS)):
+        lhsXVelo= velcmyLHS*(cm2m/year)
+        
+    elif ( y >= (bottomDepth-switchDepthLHS-20e3)):
+        lhsXVelo= velcmyLHS*(cm2m/year)*((y - (bottomDepth-switchDepthLHS-20e3))*35.0)/bottomDepth
+         
+    elif ( y >= (bottomDepth-switchDepthLHS-30e3)):
+        lhsXVelo= 0.01*velcmyLHS*(cm2m/year)*((y - (bottomDepth-switchDepthLHS-20e3))*35.0)/bottomDepth
+           
+    else:     
+        lhsXVelo= -5e-3*velcmyLHS*(cm2m/year)
+
+    csvFile.write(str(lhsXVelo)+","+str(rhsXVelo)+","+str(y)+"\n")
+# ---
+           
+csvFile.close()
+    
+    
