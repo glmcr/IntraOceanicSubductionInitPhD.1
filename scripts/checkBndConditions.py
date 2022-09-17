51e568e495b36909405cd97395cbfb7985174b10
glmcr <gmj273@gmail.com>
2022-09-17 15:10:30 -0400

    Fixed bug for RHS.

--- scripts/checkBndConditions.py
+++ scripts/checkBndConditions.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
 import sys
+import math
 
-velcmyLHS= 1.0
-velcmyRHS= -1.0
+# convergence
+velcmyLHS= 3.0
+velcmyRHS= -3.0
+
+# extension
+#velcmyLHS= -3.0
+#velcmyRHS= 3.0
 
 cm2m=0.01
 year=1
@@ -38,11 +44,12 @@ for cellY in range(700):
         rhsXVelo= velcmyRHS*(cm2m/year)*((y - (bottomDepth-switchDepthRHS-20e3))*35.0)/bottomDepth
 
     elif ( y >= (bottomDepth-switchDepthRHS-40e3)):
-        rhsXVelo= 0.1*velcmyLHS*(cm2m/year)*(((bottomDepth-switchDepthRHS-20e3)-y)*35.0)/bottomDepth
+        rhsXVelo= -0.05*velcmyRHS*(cm2m/year)*(((bottomDepth-switchDepthRHS-20e3)-y)*35.0)/bottomDepth
+        #rhsXVelo= -0.05*math.copysign(1.0,velcmyRHS)*velcmyRHS*(cm2m/year)*(((bottomDepth-switchDepthRHS-20e3)-y)*35.0)/bottomDepth
         
     else:
         #rhsXVelo= -5e-3*velcmyRHS*(cm2m/year)
-        rhsXVelo= -0.1*velcmyRHS*(cm2m/year)
+        rhsXVelo= -0.05*velcmyRHS*(cm2m/year)
 
     if (y > (bottomDepth-switchDepthLHS)):
         lhsXVelo= velcmyLHS*(cm2m/year)
@@ -51,12 +58,12 @@ for cellY in range(700):
         lhsXVelo= velcmyLHS*(cm2m/year)*((y - (bottomDepth-switchDepthLHS-20e3))*35.0)/bottomDepth
 
     elif ( y >= (bottomDepth-switchDepthLHS-40e3)):
-        #lhsXVelo= 0.01*velcmyLHS*(cm2m/year)*((y - (bottomDepth-switchDepthLHS-20e3))*35.0)/bottomDepth
-        lhsXVelo= -0.1*velcmyLHS*(cm2m/year)*(((bottomDepth-switchDepthLHS-20e3)-y)*35.0)/bottomDepth
+        lhsXVelo= -0.05*velcmyLHS*(cm2m/year)*(((bottomDepth-switchDepthLHS-20e3)-y)*35.0)/bottomDepth
+        #lhsXVelo= 0.05*math.copysign(1.0,velcmyLHS)*velcmyLHS*(cm2m/year)*(((bottomDepth-switchDepthLHS-20e3)-y)*35.0)/bottomDepth
 
     else:     
         #lhsXVelo= -5e-3*velcmyLHS*(cm2m/year)
-        lhsXVelo= -0.1*velcmyLHS*(cm2m/year)
+        lhsXVelo= -0.05*velcmyLHS*(cm2m/year)
 
     csvFile.write(str(lhsXVelo)+","+str(rhsXVelo)+","+str(y)+"\n")
 # ---
